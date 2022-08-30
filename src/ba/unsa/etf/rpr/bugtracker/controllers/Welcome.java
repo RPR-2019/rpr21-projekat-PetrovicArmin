835a24689a13359bc505b892a9336f12f21042c0
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-08-30 15:08:13 +0200

    Initial project configuration

---
+++ src/ba/unsa/etf/rpr/bugtracker/controllers/Welcome.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.bugtracker.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Welcome implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // TODO - later implement this method
+    }
+}
