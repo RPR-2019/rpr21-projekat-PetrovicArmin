c925e5a580c5f8d88755d918d7dc4fb8c8f538ab
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-09-01 14:36:53 +0200

    Closes #3, Closes#5; Created signup and login buttons. Also created about page for basic application usage.

---
+++ src/ba/unsa/etf/rpr/bugtracker/controllers/About.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.rpr.bugtracker.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class About implements Initializable {
+
+    public About() {
+
+    }
+    @Override
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
--- src/ba/unsa/etf/rpr/bugtracker/controllers/Welcome.java
+++ src/ba/unsa/etf/rpr/bugtracker/controllers/Welcome.java
@@ -3,14 +3,22 @@ package ba.unsa.etf.rpr.bugtracker.controllers;
 import ba.unsa.etf.rpr.bugtracker.common.database.Database;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class Welcome implements Initializable {
     public Button btnAbout;
+    public Button btnLogin;
+    public Button btnSignup;
     Database database;
 
     public Welcome() {
@@ -23,7 +31,22 @@ public class Welcome implements Initializable {
         // TODO - later implement this method
     }
 
-    public void showAboutPage(ActionEvent actionEvent) {
+    public void showAboutPage(ActionEvent actionEvent) throws IOException {
+        Parent root;
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/about.fxml")));
+        Stage aboutStage = new Stage();
+        aboutStage.setScene(new Scene(root, 600, 450));
+        aboutStage.setResizable(false);
+        aboutStage.setTitle("About Bug Tracker");
+        aboutStage.toFront();
+        aboutStage.show();
+    }
 
+    public void showLogin(ActionEvent actionEvent) {
+        //TODO - go from this to login window
+    }
+
+    public void showSignup(ActionEvent actionEvent) {
+        //TODO - go from this to signup window
     }
 }
