8637c0efe894e31916d20ea15671485a4832b6e9
PetrovicArmin <apetrovic1@etf.unsa.ba>
2022-09-03 07:15:59 +0200

    Closes #13; Created basic UI for login page!

--- src/ba/unsa/etf/rpr/bugtracker/controllers/Login.java
+++ src/ba/unsa/etf/rpr/bugtracker/controllers/Login.java
@@ -4,12 +4,22 @@ import ba.unsa.etf.rpr.bugtracker.common.database.Database;
 import ba.unsa.etf.rpr.bugtracker.common.other.Showable;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Login extends AbstractController implements Showable, Initializable {
     private Database database;
+    public Button btnOk;
+    public Button btnCancel;
+    public TextField fldUsernameOrEmail;
+    public PasswordField fldPassword;
 
     public Login() {
         database = Database.getInstance();
@@ -21,4 +31,11 @@ public class Login extends AbstractController implements Showable, Initializable
 
     }
 
+    public void onOk(ActionEvent actionEvent) {
+
+    }
+
+    public void onCancel(ActionEvent actionEvent) {
+        ((Stage)((Button)actionEvent.getSource()).getScene().getWindow()).close();
+    }
 }
